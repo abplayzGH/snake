9e457504dff06b9ec4e581df1aa3a2f7d377ef18
Adam Lawrence <adam@impostertwo.com>
2024-08-26 18:56:19 -0500

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,8 @@
 #include <raylib.h>
-#include "ball.h"
+#include <iostream>
 
-int main()
-{
-    Color darkGreen = Color{20, 160, 133, 255};
+using namespace std;
 
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-
-    Ball ball = Ball();
-
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-
-    while (!WindowShouldClose())
-    {
-        BeginDrawing();
-        ClearBackground(darkGreen);
-        ball.Update();
-        ball.Draw();
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
+int main(){
+    cout << "Hellow";
 }

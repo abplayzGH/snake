b863dd254fbd2c9b8566336d155cbc8f2f709354
Adam Lawrence <adam@impostertwo.com>
2024-08-26 19:25:03 -0500

    waht

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,46 @@
 
 using namespace std;
 
+Color green = {173, 204, 96, 255};
+Color darkGreen = {43, 51, 24, 255};
+
+int cellSize = 30;
+int cellCount = 25;
+
+class Food{
+    public:
+    Vector2 position = {5, 6};
+
+    void Draw(){
+        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+    }
+
+
+};
+
+
+
 int main(){
-    cout << "Hellow";
+    cout << "Starting Game!" << endl;
+    //Init
+    InitWindow(cellSize*cellCount, cellSize*cellCount, "Snake Lol");
+    SetTargetFPS(60);
+
+    //Init Objects
+
+    //Game Loop
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(green);
+
+        EndDrawing();
+    }
+    
+
+
+    //Close
+    CloseWindow();
+    return 0;
 }
